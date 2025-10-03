1c253f2d52dfaa1fe9f1ee18a978fc42c67b4697
Bagelboy69 <lloyd.bjornstedt@gmail.com>
2025-10-03 17:17:25 +0200

    Add files via upload

--- blue lobster.py
+++ blue lobster.py
@@ -1,3 +1,7 @@
+#----------------------
+# VARIABLER och LISTOR
+# ---------------------
+
 import random
 
 player_1 = []
@@ -12,6 +16,9 @@ kortlek = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
            57, 58, 59, 60]
 sakhög = []
 
+gi_min = [1,7,13,19,25,31,37,43,49,55]
+gi_max = [6,12,18,24,30,36,42,48,54,60]
+
 alla_poäng = [0, 0, 0, 0]
 
 wincon = 0
@@ -24,30 +31,110 @@ aktiva_spelare = alla_spelare[:antal_spelare]
 aktiva_poäng = alla_poäng[:antal_spelare]
 
 
+#------------
+# FUNKTIONER
+#-----------
 
-def godkända_kort(spelare):
-    sparade_kort = []
-    kort = 1
-    for j in range(0, 10):
-        if kort <= spelare[j] <= kort + 5:
-            sparade_kort.append(spelare[j])
+def intervall_check(spelare):
+    godkända_kort = []
+    for k in range(0,10):
+        if gi_min[k] < spelare[k] < gi_max[k]:
+            godkända_kort.append(spelare[k])
         else:
-            sparade_kort.append(False)
-        kort += 6
+            godkända_kort.append(False)
+    return godkända_kort
 
-    return sparade_kort
 
-def god_kända_visa(spelare):
-    sparade_kort = []
-    kort = 1
-    for j in range(0, 10):
-        if kort <= spelare[j] <= kort + 5:
-            print(j + 1, ": Godkänt")
-        else:
-            print(j + 1, ": Icke godkänd")
-        kort += 6
+def blanda_kort(hög):
+    random.shuffle(hög)
+    return hög
 
-    return sparade_kort
+
+def visa_kort(hand):
+    for kort in hand:
+        print(hand.index(kort) + 1, ":", kort)
+
+
+def kortutdelning(alla_spelare, lek, sakhög):
+    blanda_kort(lek)
+    for spelare in alla_spelare:
+        for _ in range(10):
+            kort = lek.pop(0)
+            spelare.append(kort)
+    sakhög.append(lek.pop(0))
+    return alla_spelare
+
+
+def poäng_bricka(spelare):
+    poäng = 5
+    for i in range(0, 9):
+        if spelare[i] < spelare[i + 1]:
+            poäng += 5
+        elif spelare[i] > spelare[i + 1]:
+            break
+    return poäng
+
+
+def Special_Racko(x):
+    if x > 6:
+        x = 6
+    bonuspoäng = 50 * (2 ** (x - 3))
+    return bonuspoäng
+
+
+def Räkna_Poäng(aktiva_poäng, spelare, aktiva_spelare):
+    for tur in range(len(aktiva_spelare)):
+        poäng = aktiva_poäng[tur]
+        if aktiva_spelare[tur] == spelare:
+            poäng += 25
+            count = 1
+            consecutive_runs_list = []
+            for i in range(9):
+                if spelare[i + 1] == spelare[i] + 1:
+                    count += 1
+                else:
+                    consecutive_runs_list.append(count)
+                    count = 1
+            highest_consecutive = max(consecutive_runs_list)
+            if highest_consecutive >= 3:
+                poäng += Special_Racko(highest_consecutive)
+        print(spelare_namn[tur])
+        poäng += poäng_bricka(aktiva_spelare[tur])
+        print("Poäng: ", poäng)
+        aktiva_poäng[tur] = poäng
+
+
+def spelrunda(spelare, kortlek, sakhög):
+    poäng_bricka(spelare)
+    print("")
+    visa_kort(spelare)
+    print("Sakhög:", sakhög[-1])
+
+    val = (input("Ta upp? ja/nej: "))
+    if val == "ja" or val == "Ja":
+        byta = int(input("Byta (index): "))
+        spelare.insert(byta - 1, sakhög.pop(-1))
+        sakhög.append(spelare.pop(byta))
+        visa_kort(spelare)
+        print("\n\n")
+
+
+    elif val == "nej" or val == "Nej":
+        print("\n")
+        print("Kortlek:", kortlek[0], " ", "Sakhög:", sakhög[-1])
+        val = (input("Behålla ja/nej: "))
+
+        if val == "ja" or val == "Ja":
+            byta = int(input("Byta (index): "))
+            spelare.insert(byta - 1, kortlek.pop(0))
+            sakhög.append(spelare.pop(byta))
+            print(visa_kort(spelare), "\n", "\n")
+            print("")
+
+        elif val == "nej" or val == "Nej":
+            sakhög.append(kortlek.pop(0))
+            print("")
+
 
 def Adjust_Interval(approved_cards):
     approved_cards_stripped = []
@@ -96,131 +183,47 @@ def Adjust_Interval(approved_cards):
 
     return intervals
 
-def kortcheck(intervals,kort):
-    rätt_plats = []
-    for j in range(0,9):
-        if intervals[j] < kort <= intervals[j+1]:
-            #if intervals[j+1] != False:
-            rätt_plats.append(j+2)
-        #if len(rätt_plats)==0:
-            #return "ta från kortleken"
-    return kort,"passar i",rätt_plats[-1]
 
-def blanda_kort(hög):
-    random.shuffle(hög)
-    return hög
-
-
-def visa_kort(hand):
-    for kort in hand:
-        print(hand.index(kort) + 1, ":", kort)
-
-
-def kortutdelning(alla_spelare, lek, sakhög):
-    blanda_kort(lek)
-    for spelare in alla_spelare:
-        for _ in range(10):
-            kort = lek.pop(0)
-            spelare.append(kort)
-    sakhög.append(lek.pop(0))
-    return alla_spelare
-
-
-# fixa så att första kortet i kortleken efter utdelning hamnar i sakhögen
-
-def poäng_bricka(spelare):
-    poäng = 5
-    for i in range(0, 9):
-        if spelare[i] < spelare[i + 1]:
-            poäng += 5
-        elif spelare[i] > spelare[i + 1]:
+def bot1(godkända_kort,kort1,kort2,spelare):
+    for j in range(0, 10):
+        if godkända_kort[j] == False and gi_min[j] <= kort1[-1] <= gi_max[j]:
+            print("botten tar",kort1[-1],"från sakhögen")
+            spelare.insert(j, kort1[-1])
+            kort1.pop(-1)
+            kort1.append(spelare[j])
+            spelare.pop(j - 1)
             break
-    return poäng
 
-
-def Special_Racko(x):
-    if x > 6:
-        x = 6
-    bonuspoäng = 50 * (2 ** (x - 3))
-    return bonuspoäng
-    # failsafehantering är att 3 <= x <= 6
-
-
-def Räkna_Poäng(aktiva_poäng, spelare, aktiva_spelare):
-    for tur in range(len(aktiva_spelare)):
-        #separat för spelaren som vann:
-        poäng = aktiva_poäng[tur]
-        if aktiva_spelare[tur] == spelare:
-            poäng += 25
-            count = 1
-            consecutive_runs_list = []
-            for i in range(9):
-                if spelare[i + 1] == spelare[i] + 1:
-                    count += 1
-                else:
-                    consecutive_runs_list.append(count)
-                    count = 1
-            highest_consecutive = max(consecutive_runs_list)
-            if highest_consecutive >= 3:
-                poäng += Special_Racko(highest_consecutive)
-        print(spelare_namn[tur])
-        poäng += poäng_bricka(aktiva_spelare[tur])
-        print("Poäng: ", poäng)
-        aktiva_poäng[tur] = poäng
-
-
-def spelrunda(spelare, kortlek, sakhög):
-    poäng_bricka(spelare)
-    god_kända_visa(aktiva_spelare[tur])
-    print(Adjust_Interval(godkända_kort(aktiva_spelare[tur])))
-    print(kortcheck(Adjust_Interval(Adjust_Interval(godkända_kort(aktiva_spelare[tur]))), sakhög[-1]))
-    print("")
-    visa_kort(spelare)
-    print("Sakhög:", sakhög[-1])
-
-    val = (input("Ta upp? ja/nej: "))
-    if val == "ja" or val == "Ja":
-        byta = int(input("Byta (index): "))
-        spelare.insert(byta - 1, sakhög.pop(-1))
-        sakhög.append(spelare.pop(byta))
-        visa_kort(spelare)
-        print("\n\n")
-
-
-    elif val == "nej" or val == "Nej":
-        print(kortcheck(Adjust_Interval(Adjust_Interval(godkända_kort(aktiva_spelare[tur]))), kortlek[0]))
-        print("\n")
-        print("Kortlek:", kortlek[0], " ", "Sakhög:", sakhög[-1])
-        # Behövs sakhögen också printas ut? man kommer ju inte kunna komma åt det/ ta upp det ändå
-        val = (input("Behålla ja/nej: "))
-
-        if val == "ja" or val == "Ja":
-            byta = int(input("Byta (index): "))
-            spelare.insert(byta - 1, kortlek.pop(0))
-            sakhög.append(spelare.pop(byta))
-            print(visa_kort(spelare), "\n", "\n")
-            print("")
-
-        elif val == "nej" or val == "Nej":
-            sakhög.append(kortlek.pop(0))
-            print("")
+        elif godkända_kort[j] == False and gi_min[j] <= kort2[0] <= gi_max[j]:
+            print("botten tar",kort2[0], "från kortlekten")
+            spelare.insert(j, kort2[0])
+            kort2.pop(0)
+            kort2.append(spelare[j])
+            spelare.pop(j-1)
+            break
 
 
 
-"""
-def bot1():
-    print("bot1")
 
-def bot2():
-    print("bot2")
-"""
+
+
+
 
 aktiva_spelare = kortutdelning(aktiva_spelare, kortlek, sakhög)
 while 500 not in aktiva_poäng:
     while wincon < 1:
         for tur in range(0, antal_spelare):
             print(spelare_namn[tur], "'s tur", sep='')
-            spelrunda(aktiva_spelare[tur], kortlek, sakhög)
+
+            if spelare_namn[tur] == "bot":
+                print(aktiva_spelare[tur])
+                print(sakhög[-1],kortlek[0])
+                bot1(intervall_check(aktiva_spelare[tur]),sakhög,kortlek,aktiva_spelare[tur])
+                print(aktiva_spelare[tur])
+
+            else:
+                spelrunda(aktiva_spelare[tur], kortlek, sakhög)
+
             if aktiva_spelare[tur] == sorted(aktiva_spelare[tur]):
                 svar = input("Du har Rack-o! \n\nVill du fortsätta?\n")
                 if svar == "nej" or svar == "Nej":
@@ -228,10 +231,12 @@ while 500 not in aktiva_poäng:
                     print("\nRundan har avslutats då", spelare_namn[tur],
                           "har fått Rack-o!\n\nHär är den nuvarande poängställningen:")
                     Räkna_Poäng(aktiva_poäng, aktiva_spelare[tur], aktiva_spelare)
-                    # vinnare = alla_poäng.index(max(alla_poäng))
-                    # print("\nDetta betyder att", spelare_namn[vinnare], "har vunnit!")
                     wincon += 1
                     break
+    break
+
+
+
 
 # Rack-o 75p
 # 3kort = 50p
